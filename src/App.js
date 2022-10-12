999467f049e712984fb4b36236b8de0fae541dc3
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-10-12 18:41:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import {createBrowserRouter} from 'react-router-dom'
 import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>
+    }
+  ])
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
